9065538f7c5d54a86cee8dd3488d745fa262cdfe
Natanael Pacheco <natepacheco716@gmail.com>
2022-03-18 01:35:46 -0500

    Replaced default src folder with my own & completed navbar component.

---
+++ booksearch/src/components/Navbar/Navbar.component.jsx
@@ -0,0 +1,13 @@
+import './Navbar.styles.css'
+
+
+const Navbar = () => {
+const testURL = '#';
+    return (
+        <nav className='nav-container'>
+        <a className='nav-logo' href={testURL}>Bookish</a>
+        </nav>
+  )
+}
+
+export default Navbar
